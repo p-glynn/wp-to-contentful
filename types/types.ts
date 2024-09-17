de79f1a73839215808817067cde00913b002dab8
Patrick Glynn <pglynnco@gmail.com>
2024-09-17 14:21:28 -0600

    continue adding types

--- types/types.ts
+++ types/types.ts
@@ -1,3 +1,67 @@
-export interface WpData {
-  questions: unknown[];
+import { localeString, optionalProperties } from '../src/constants';
+
+export interface WPQueryOptions {
+    baseUrl: string;
+    page: number;
+    pageSize: number;
+    questionType?: string;
 }
+
+export interface WPAuthHeaders {
+    headers?: {
+        Authorization: string;
+    };
+}
+
+export interface WPResponse {
+    data?: Question[];
+}
+
+export interface Question extends OptionalMediaProps<typeof optionalProperties> {
+    id: number;
+    question_title: string;
+    question_type: string;
+    explanation: string;
+}
+
+export interface ContentfulQuestion {
+    fields: {
+        questionTitle: {
+            [localeString]: string;
+        };
+        questionType: {
+            [localeString]: string;
+        };
+        wordpressId: {
+            [localeString]: number;
+        };
+        explanation: {
+            [localeString]: string;
+        };
+        images?: {
+            [localeString]: AssetLink[];
+        };
+        markedImages?: {
+            [localeString]: AssetLink[];
+        };
+        videos?: {
+            [localeString]: AssetLink[];
+        };
+    };
+}
+
+export interface Media {
+    contentType: string;
+    fileName: string;
+    url: string;
+}
+
+export interface AssetLink {
+    sys: {
+        id: string;
+    };
+}
+
+type OptionalMediaProps<T extends readonly string[]> = {
+    [K in T[number]]?: string | number;
+};
