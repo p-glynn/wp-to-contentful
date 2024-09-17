0bdf88c5f7fe8b6c2ea3f0a3ea1aea82c3e97276
Patrick Glynn <pglynnco@gmail.com>
2024-09-17 14:21:12 -0600

    re-write writeDataToFile to be async, add delay function

--- src/_utils.ts
+++ src/_utils.ts
@@ -1,25 +1,39 @@
-import { writeFile } from 'fs';
+import fs from 'fs/promises';
+import path from 'path';
 
 /**
  * Write data to its own JSON file.
  * @param {Object} inputData - JSON data to write to file
- * @param {string} label - name of the file to write to
+ * @param {string} subdirectory - name of the file to write to
  */
-export function writeDataToFile(inputData: object, label: string, { isError = false, fetchAll = false }) {
-    console.log(`Writing ${label} data to file`);
+export async function writeDataToFile(inputData: object, subdirectory: string, fileName: string, append = false) {
+    console.log(`Writing ${fileName} data to file`);
 
-    let outputDirectory = fetchAll ? 'full' : 'test';
-    if (isError) {
-        outputDirectory += '/errors';
-    }
+    const filePath = path.join('output', subdirectory, `${fileName}.json`);
 
-    const filePath = `./output/${outputDirectory}/${label}.json`;
-
-    writeFile(filePath, JSON.stringify(inputData, null, 2), (err) => {
-        if (err) {
-            console.error(err);
-            return;
+    try {
+        let exists = false;
+        try {
+            await fs.access(filePath);
+            exists = true;
+        } catch {
+            exists = false;
         }
-        console.log(`Finished writing ${label} data`);
-    });
+
+        if (!exists) {
+            await fs.mkdir(filePath, { recursive: true });
+        }
+        if (append) {
+            await fs.appendFile(filePath, JSON.stringify(inputData, null, 2));
+        } else {
+            await fs.writeFile(filePath, JSON.stringify(inputData, null, 2));
+        }
+        console.log(`Finished writing ${subdirectory}/${fileName} data`);
+    } catch (error) {
+        console.error(`Error writing file: ${error}`);
+    }
+}
+
+export function delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
