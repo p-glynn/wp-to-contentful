5d3209d7653ad213995b4a96e8e1bae7eb02f05a
Patrick Glynn <pglynnco@gmail.com>
2024-09-17 21:44:56 -0600

    define media properties

--- src/constants.ts
+++ src/constants.ts
@@ -28,8 +28,34 @@ export const wpEndpoints = {
     questions: [],
 };
 
-// can re-label to "customPostTypes" before finalizing?
 export const questionTypes = ['angiogram', 'ecg', 'echo', 'cv_image'];
+
+export const imageProperties = [
+    'question_ecg_image',
+    'question_ecg_image_2',
+    'question_ecg_image_3',
+    'question_ecg_image_4',
+    'question_ecg_image_5',
+];
+
+export const markedImageProperties = [
+    'question_ecg_image_marked',
+    'question_ecg_image_marked_2',
+    'question_ecg_image_marked_3',
+    'question_ecg_image_marked_4',
+    'question_ecg_image_marked_5',
+];
+
+export const videoProperties = [
+    'question_ecg_video',
+    'question_ecg_video_2',
+    'question_ecg_video_3',
+    'question_ecg_video_4',
+    'question_ecg_video_5',
+    'question_ecg_video_6',
+];
+
+export const optionalProperties = [...imageProperties, ...markedImageProperties, ...videoProperties];
 
 // Contentful
 export const localeString = 'en-US';
